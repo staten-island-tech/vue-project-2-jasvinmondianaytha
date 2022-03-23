1e34066801a205b99cf4bb61187a8e6d2f7f1987
Ray Lian <raylian1206@gmail.com>
2022-03-23 10:11:17 -0400

    api working les goooo

--- src/components/PokeCard.vue
+++ src/components/PokeCard.vue
@@ -1,10 +1,10 @@
 <template>
-    <article class="pokemon-card">
-        <div class="pokemon-container">
-            <div class="poke-media-container">
-                <img src="https://play.pokemonshowdown.com/sprites/gen1/abra.png" />
-            </div>
-            <pre class="poke-info-container">
+  <article class="pokemon-card">
+    <div class="pokemon-container">
+      <div class="poke-media-container">
+        <img src="https://play.pokemonshowdown.com/sprites/gen1/abra.png" />
+      </div>
+      <pre class="poke-info-container">
 <span class="type-fire">Abra</span> @ Choice Specs  
 <span class="attr">Ability: </span>Water Absorb  
 <span class="attr">EVs: </span><span class="stat-spa">252 SpA</span> / <span class="stat-spd">4 SpD</span> / <span class="stat-spe">252 Spe</span>  
@@ -15,26 +15,48 @@ Timid Nature
 <span class="type-ground">-</span> Earth Power  
 <span class="type-poison">-</span> Sludge Wave  
     </pre>
-        </div>
+    </div>
 
-        <div
-            class="desc"
-        >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum, similique voluptatem exercitationem culpa, incidunt nam eos aut dolor tenetur ratione non inventore recusandae? Reiciendis optio recusandae ex dolor quia totam.</div>
-    </article>
+    <div class="desc">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum,
+      similique voluptatem exercitationem culpa, incidunt nam eos aut dolor
+      tenetur ratione non inventore recusandae? Reiciendis optio recusandae ex
+      dolor quia totam.
+    </div>
+    <button @click="ok()"></button>
+  </article>
 </template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const result = ref(null);
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((response) => response.json())
+      .then((data) => (result.value = data));
+    return { result };
+  },
+  methods: {
+    ok() {
+      console.log(this.result.results);
+    },
+  },
+};
+</script>
 
 <style scoped>
 .pokemon-card {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 .pokemon-container {
-    display: flex;
+  display: flex;
 }
 .poke-media-container img {
-    width: 15rem;
+  width: 15rem;
 }
 .desc {
-    width: 16rem;
+  width: 16rem;
 }
 </style>
