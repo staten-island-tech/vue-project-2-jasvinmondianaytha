33f61f0997ffbbce147dc766efd20972069a454d
Alvin Mei <alvinkmei@gmail.com>
2022-04-04 22:20:49 -0400

    psuhhhh

--- src/components/StatForm.vue
+++ src/components/StatForm.vue
@@ -1,4 +1,38 @@
-<template></template>
+<script setup>
+import StatRow from './StatRow.vue';</script>
+<template>
+    <table class="wrapper">
+        <tr class="mini-caption">
+            <td>
+                <label id="label-attribute">Attribute</label>
+            </td>
+            <td class="mini-cap-contents">Base</td>
+            <td class="mini-cap-contents">IVs</td>
+            <td class="mini-cap-contents">EVs</td>
+            <td class="mini-cap-contents" title="Positive/Negative Nature">Nat</td>
+            <td class="mini-cap-contents" title="In-battle Value">Val</td>
+        </tr>
+        <StatRow attr="hp" />
+        <StatRow attr="atk" />
+        <StatRow attr="def" />
+        <StatRow attr="spatk" />
+        <StatRow attr="spdef" />
+        <StatRow attr="spd" />
+    </table>
+</template>
 
 <style scoped>
+.wrapper label {
+    width: 48px;
+    display: inline-block;
+}
+.base {
+    margin-left: 4px;
+    width: 30px;
+}
+.mini-cap-contents {
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+}
 </style>
