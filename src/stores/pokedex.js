302b69eaaf194e19dad34550666bf64de4f2bf7b
Alvin Mei <alvinkmei@gmail.com>
2022-04-13 13:34:00 -0400

    uwauwauuwaua

--- src/stores/pokedex.js
+++ src/stores/pokedex.js
@@ -1,23 +1,45 @@
 import { defineStore } from "pinia";
-
-const pokedexUrl = "https://pokeapi.co/api/v2";
+import { pokemons } from "@/assets/pokemon.json";
 
 export const usePokedexStore = defineStore({
   id: "pokedex",
   state: () => ({
     pokemons: [],
+    genSel: 0,
   }),
   getters: {
-    getGen1() {
-      return this.pokemons;
+    gen() {
+      switch (this.genSel) {
+        case 0:
+          return this.pokemons;
+        case 1:
+          return this.pokemons.slice(0, 151);
+        case 2:
+          return this.pokemons.slice(151, 251);
+        case 3:
+          return this.pokemons.slice(251, 386);
+        case 4:
+          return this.pokemons.slice(386, 493);
+        case 5:
+          return this.pokemons.slice(493, 649);
+        case 6:
+          return this.pokemons.slice(649, 721);
+        case 7:
+          return this.pokemons.slice(721, 809);
+        case 8:
+          return this.pokemons.slice(809, 905);
+
+        default:
+          return this.pokemons;
+      }
     },
   },
   actions: {
-    async fetchAll() {
-      const res = await fetch(`${pokedexUrl}/pokemon?limit=898&offset=0`);
-      const data = await res.json();
-      this.pokemons = data.results.map((pokemon) => pokemon);
-      console.log(this.pokemons);
+    fetchAll() {
+      this.pokemons = pokemons.map((pokemon) => pokemon);
+    },
+    setGen(n) {
+      this.genSel = n;
     },
   },
 });
