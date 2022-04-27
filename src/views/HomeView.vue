543a167ff55e755d5ad66feda645c54e5cc6c69d
Ray Lian <raylian1206@gmail.com>
2022-04-27 10:10:04 -0400

    aaaaaaaaaasaaaaaaaa

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,53 @@
-<script setup></script>
+<script>
+export default {
+  data() {
+    let width = 100;
+    return { width };
+  },
+  computed: {
+    computedWidth: function () {
+      return this.width.toString() + "%";
+    },
+  },
+  methods: {
+    hungry: function () {
+      setInterval(() => {
+        if (this.width <= 100) {
+          this.width = this.width - 20;
+        } else if (this.width === 0) {
+          return;
+        }
+      }, 1000);
+    },
+    full: function () {
+      setInterval(() => {
+        if (this.width < 100) {
+          this.width = this.width + 20;
+        } else if (this.width === 100) {
+          return;
+        }
+      }, 1000);
+    },
+  },
+};
+</script>
 
 <template>
-  <main></main>
+  <div id="greyBar">
+    <div id="greenBar" v-bind:style="{ width: computedWidth }">Hunger Bar</div>
+  </div>
+  <button @click="hungry">Get hungry</button>
+  <button @click="full">Get full</button>
 </template>
+
+<style>
+#greyBar {
+  width: 100%;
+  background-color: grey;
+}
+
+#greenBar {
+  height: 30px;
+  background-color: green;
+}
+</style>
